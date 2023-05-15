07e033feb3684fa39c73296d64f9e3c9fdb43643
AndresLex <andrescamargocisf@gmail.com>
2023-05-15 11:29:26 -0500

    6 Commit

--- src/main/java/com/example/ProyectoGym/Controller/Controlador.java
+++ src/main/java/com/example/ProyectoGym/Controller/Controlador.java
@@ -1,13 +1,31 @@
 package com.example.ProyectoGym.Controller;
 
-
+import com.example.ProyectoGym.InterfaceService.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import  org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
 public class Controlador {
+    @Autowired
+    private IUsuarioService servUsu;
+    @Autowired
+    private IRolService servRol;
+    @Autowired
+    private IPagoService servPag;
+    @Autowired
+    private IMembresiaService servMem;
+    @Autowired
+    private IControlIngresoService servCont;
 
-
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("title", "Inicio de sesion");
+        model.addAttribute("txt", "Login");
+        return "login";
+    }
 
 }
