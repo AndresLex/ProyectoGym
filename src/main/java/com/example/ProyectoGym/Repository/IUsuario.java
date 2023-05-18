274c985481c8de771ad0c513ca6006eba8d26ace
andrexexe <andresdmendez@gmail.com>
2023-05-18 10:13:34 -0500

    Clase

--- src/main/java/com/example/ProyectoGym/Repository/IUsuario.java
+++ src/main/java/com/example/ProyectoGym/Repository/IUsuario.java
@@ -2,9 +2,13 @@ package com.example.ProyectoGym.Repository;
 
 import com.example.ProyectoGym.Model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IUsuario extends JpaRepository<Usuario, Integer> {
-
+   /* @Query("SELECT id_rol FROM usuario")
+    List<Usuario> findAll();*/
 }
